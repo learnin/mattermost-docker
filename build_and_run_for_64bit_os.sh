5d1018e1f8b6667242bb77ace088b73f273f494b
Manabu Inoue <inoue.manabu@tis.co.jp>
2016-08-05 14:31:34 +0900

    [fix]Fix error on create mysql full text index

--- build_and_run_for_64bit_os.sh
+++ build_and_run_for_64bit_os.sh
@@ -6,4 +6,7 @@ docker-compose up -d
 
 # Enable cjk search
 sleep 5
-docker exec mattermostdocker_db_1 /create_fulltext_index.sh
+MYSQL_DATABASE=`docker inspect -f='{{range $v := .Config.Env}}{{println $v}}{{end}}' mattermostdocker_db_1 | grep MYSQL_DATABASE | sed 's/MYSQL_DATABASE=//'`
+MYSQL_ROOT_PASSWORD=`docker inspect -f='{{range $v := .Config.Env}}{{println $v}}{{end}}' mattermostdocker_db_1 | grep MYSQL_ROOT_PASSWORD | sed 's/MYSQL_ROOT_PASSWORD=//'`
+
+docker exec mattermostdocker_db_1 /create_fulltext_index.sh $MYSQL_DATABASE $MYSQL_ROOT_PASSWORD
